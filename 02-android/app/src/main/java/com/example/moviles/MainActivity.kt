cb5613b366cdb38bb0f662a10106b60d5ce2616a
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-07-06 09:02:30 -0500

    ciclo de vida e interfaz

--- 02-android/app/src/main/java/com/example/moviles/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/moviles/MainActivity.kt
@@ -1,47 +1,36 @@
 package com.example.moviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("Activity", "OnCreate")
-    }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i("Activity", "OnStart")
-    }
+        btn_ciclo_vida.setOnClickListener { boton ->
+            irCicloDeVida()
 
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("Activity", "onRestart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("Activity", "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("Activity", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("Activity", "onStop")
+        }
     }
 
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("Activity", "onDestroy")
+    //ir ciclo de vida
+
+    fun irCicloDeVida(){
+        val intentExplicito = Intent(
+            this,
+            CicloVida::class.java
+
+        )
+
+        this.startActivity(intentExplicito)
     }
+
 }
 
 
