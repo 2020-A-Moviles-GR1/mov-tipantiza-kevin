513ecb558535af17eb40715d3bac3412ead46e9d
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-08-13 10:33:37 -0500

    hasta probar estandar restfull

--- 03-Backend/mascotas/api/controllers/UsuarioController.js
+++ 03-Backend/mascotas/api/controllers/UsuarioController.js
@@ -6,7 +6,64 @@
  */
 
 module.exports = {
-  
+
 
 };
 
+//Servicio - Resfull
+//HTTP
+
+//URL METODO PARAMETROS
+
+//PARAMETROS DE RUTA
+
+//http://localhost:1337/usuario/1
+//http://localhost:1337/usuario/2
+//http://localhost:1337/usuario/4/casa/1
+//http://localhost:1337/usuario/4/casa/2
+//http://localhost:1337/usuario/4/casa/5
+
+//PARAMETROS DE CONSULTA
+
+//http://localhost:1337/usuario?=nombre=kevin&apellido=tipantiza&edad=20
+//nombre -> Kevin
+//apellido -> tipantiza
+//edad -> 20
+
+//PARAMETROS DE CUERPO (EXCEPTO EN GET)
+
+//GET 
+//PUT
+//DELETE
+//POST
+
+
+//ESTANDAR Resful
+
+// 1- Buscar todos
+// 2- Buscar uno
+// 3- Crear uno 
+// 4- Editar uno 
+// 5- Eliminar uno
+
+//comprobar si sails es un api resful
+
+//BUSCAR TODOS
+
+//GET http://localhost:1337/Usuario
+
+//BUSCAR UNO 
+
+//GET http://localhost:1337/Usuario/1
+
+//CREAR UNO 
+
+//POST http://localhost:1337/Usuario (Parametros de cuerpo)
+
+//EDITAR UNO 
+
+//PUT http://localhost:1337/Usuario/6 (Parametros de cuerpo)
+
+//Eliminar Uno
+
+//DELETE http://localhost:1337/Usuario/3
