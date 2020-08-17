f235db79b57500a56006f82afd99610ee4d4a948
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-08-17 08:58:28 -0500

    hasta relacionar

---
+++ 03-Backend/mascotas/api/models/Pokemon.js
@@ -0,0 +1,20 @@
+/**
+ * Pokemon.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+        nombre: {
+            type: 'string'
+        },
+        usuario: { //RELACION DE UNO A MUCHOS (NOMBRE FK) -MISMO NOMBRE QUE LA RELACION
+            model: 'usuario',
+            required: true // (Es opcional 1 muchos 0 muchos)
+        }
+    },
+
+};
