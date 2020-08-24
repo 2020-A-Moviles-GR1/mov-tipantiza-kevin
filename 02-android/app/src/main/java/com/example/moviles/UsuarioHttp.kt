1f5488014cd82c0888520007771a821925a4cde6
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-08-24 09:05:00 -0500

    dependencias y codigo http

---
+++ 02-android/app/src/main/java/com/example/moviles/UsuarioHttp.kt
@@ -0,0 +1,17 @@
+package com.example.moviles
+
+class UsuarioHttp(
+    var id: Int,
+    var createdAt: Long,
+    var updateAt: Long,
+    var cedula: String,
+    var nombre: String,
+    var correo:String,
+    var estadoCivil:String,
+    var password:String
+){
+
+
+
+
+}
