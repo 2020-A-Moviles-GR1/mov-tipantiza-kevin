4149f1aeb2d9d8f909ddff02b1a430184fb8e55c
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-08-29 15:19:08 -0500

    Deber klaxon

--- 02-android/app/src/main/java/com/example/moviles/PokemonHttp.kt
+++ 02-android/app/src/main/java/com/example/moviles/PokemonHttp.kt
@@ -1,5 +1,9 @@
 package com.example.moviles
 
+import com.beust.klaxon.Converter
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.JsonValue
+import com.beust.klaxon.Klaxon
 import java.util.*
 
 class PokemonHttp (
@@ -8,7 +12,7 @@ class PokemonHttp (
     var id: Int,
     var nombre: String,
     var usuario:Int
-
+   //var usuario: Any?
 ){
     var fechaCreacion: Date
     var fechaActualizacion: Date
@@ -17,4 +21,75 @@ class PokemonHttp (
         fechaCreacion = Date(createdAt)
         fechaActualizacion = Date( updatedAt)
     }
+
+/*
+//DEBER KLAXON
+
+    companion object {
+        val myConverter = object: Converter {
+            override fun canConvert(cls: Class<*>) = cls == PokemonHttp::class.java
+
+            override fun toJson(value: Any): String {
+
+                val pokemon = value as PokemonHttp
+
+                var usuario: Any?
+
+                if(pokemon.usuario is Int){
+                    usuario = pokemon.usuario
+                }else if(pokemon.usuario is UsuarioHttp){
+                    usuario = Klaxon().toJsonString(pokemon.usuario as UsuarioHttp)
+                }else{
+                    throw Error("No soportado")
+                }
+
+                return """
+                  {
+                    "id": ${pokemon.id},
+                    "createdAt": ${pokemon.createdAt},
+                    "updatedAt": ${pokemon.updatedAt},
+                    "nombre": "${pokemon.nombre}",
+                    "usuario": ${usuario}
+                   }
+                """.trimMargin()
+
+
+            }
+
+            override fun fromJson(jv: JsonValue) : PokemonHttp {
+
+                if(jv.obj?.get("usuario") is JsonObject){
+
+                    return PokemonHttp(
+                        jv.obj?.get("createdAt") as Long,
+                        jv.obj?.get("updatedAt") as Long,
+                        jv.objInt("id"),
+                        jv.objString("nombre"),
+                        Klaxon().parseFromJsonObject<UsuarioHttp>(jv.obj?.get("usuario") as JsonObject)
+
+                    )
+                }else if(jv.obj?.get("usuario") is Int){
+                    return PokemonHttp(
+                        jv.obj?.get("createdAt") as Long,
+                        jv.obj?.get("updatedAt") as Long,
+                        jv.objInt("id"),
+                        jv.objString("nombre"),
+                        jv.objInt("usuario")
+                    )
+                }else{
+                    throw Error("Error")
+                }
+
+
+            }
+
+
+
+
+        }
+    }
+
+//DEBER KLAXON
+*/
+
 }
--- 02-android/app/src/main/java/com/example/moviles/UsuarioHttp.kt
+++ 02-android/app/src/main/java/com/example/moviles/UsuarioHttp.kt
@@ -1,5 +1,9 @@
 package com.example.moviles
 
+import com.beust.klaxon.Converter
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonValue
+import com.beust.klaxon.Klaxon
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -13,6 +17,7 @@ class UsuarioHttp(
     var estadoCivil:String,
     var password:String,
     var pokemons: ArrayList<PokemonHttp>
+   // var pokemons: ArrayList<PokemonHttp>? = null
 ){
     var fechaCreacion: Date
     var fechaActualizacion: Date
@@ -21,4 +26,72 @@ class UsuarioHttp(
         fechaCreacion = Date(createdAt)
         fechaActualizacion = Date( updatedAt)
     }
+/*
+    //DEBER KLAXON
+
+    companion object {
+        val myConverter = object: Converter {
+            override fun canConvert(cls: Class<*>) = cls == UsuarioHttp::class.java
+
+            override fun toJson(value: Any): String {
+                val usuario = value as UsuarioHttp
+
+
+                return """
+                  {
+
+                    "id": ${usuario.id},
+                    "createdAt": ${usuario.createdAt},
+                    "updatedAt": ${usuario.updatedAt},
+                    "cedula": "${usuario.cedula}",
+                    "nombre": "${usuario.nombre}",
+                    "correo": "${usuario.correo}",
+                    "estadoCivil": "${usuario.estadoCivil}",
+                    "password": "${usuario.password}",
+                    "pokemons": ${Klaxon().toJsonString(usuario.pokemons as List<*>)}
+                   }
+
+                    }
+                """.trimMargin()
+
+
+
+            }
+
+
+
+            override fun fromJson(jv: JsonValue) : UsuarioHttp {
+
+                return UsuarioHttp(
+                    jv.objInt("id"),
+                    jv.obj?.get("createdAt") as Long,
+                    jv.obj?.get("updatedAt") as Long,
+                    jv.objString("cedula"),
+                    jv.objString("nombre"),
+                    jv.objString("correo"),
+                    jv.objString("estadoCivil"),
+                    jv.objString("password"),
+                    Klaxon().parseFromJsonArray<PokemonHttp>(jv.obj?.get("pokemons") as JsonArray<*>) as ArrayList<PokemonHttp>
+
+                )
+
+
+            }
+
+
+
+
+        }
+    }
+
+
+    override fun toString(): String {
+        return "id=$id, createdAt=$createdAt, updatedAt=$updatedAt, cedula='$cedula', nombre='$nombre', correo='$correo', estadoCivil='$estadoCivil', password='$password', fechaCreacion=$fechaCreacion, fechaActualizacion=$fechaActualizacion)" + (if(pokemons!=null) "pokemons=$pokemons" else "")
+    }
+
+
+// DEBER KLAXON
+
+*/
+
 }
