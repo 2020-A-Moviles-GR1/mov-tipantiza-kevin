2a91897454a253a27f197a67ad0854fdf164a32c
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-09-10 08:48:05 -0500

    Mapas

--- 02-android/app/src/main/java/com/example/moviles/MapsActivity.kt
+++ 02-android/app/src/main/java/com/example/moviles/MapsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.moviles
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,11 +17,15 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    var tienePermisos = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        solicitarPermisos()
+
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -34,10 +42,78 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        //1) Permisos
+        establecerConfiguracionMapa(mMap)
+        val cci = LatLng(-0.178067, -78.485594)
+        val puntoUsuario = LatLng(-0.179204, -78.484189)
+        val titulo="cci"
+        val zoom = 17f
+        anadirMarcador(cci,titulo)
+        moverCamaraConZoom(puntoUsuario, zoom)
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //val sydney = LatLng(-34.0, 151.0)
+       // mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    fun moverCamaraConZoom(latLng: LatLng, zoom:Float =10f){
+        mMap.moveCamera(
+            CameraUpdateFactory
+                .newLatLngZoom(latLng, zoom)
+        )
+    }
+
+    fun anadirMarcador(latLng: LatLng, title: String){
+        mMap.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title(title)
+        )
+    }
+
+    fun establecerConfiguracionMapa(mapa:GoogleMap) {
+        val contexto = this.applicationContext
+        with(mapa) {
+            val permisosFineLocation = ContextCompat.checkSelfPermission(
+                contexto,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            val tienePermisos = permisosFineLocation == PackageManager.PERMISSION_GRANTED
+            if (tienePermisos) {
+                Log.i("mapa", "Tiene permisos FINE LOCATION")
+                mapa.isMyLocationEnabled = true
+            }
+            uiSettings.isZoomControlsEnabled = true
+            uiSettings.isMyLocationButtonEnabled = true
+
+        }
+    }
+
+    fun solicitarPermisos(){
+        val permisosFineLocation = ContextCompat
+            .checkSelfPermission(
+                this.applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+
+        val tienePermisos = permisosFineLocation ==PackageManager.PERMISSION_GRANTED
+
+        if(tienePermisos){
+
+            Log.i("mapa","tiene permisos FINE LOCATION")
+            this.tienePermisos = true
+
+        }else{
+            ActivityCompat.requestPermissions(
+                this,  //contexto
+                arrayOf( //Arreglo Permisos
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                1 //codigo que esperamos
+            )
+
+        }
+
     }
 }
